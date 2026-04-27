594c55e4d3790a0426950ec4e590abe6dd551e18
Minseo Shin <minseo9451@gmail.com>
2026-04-27 14:39:35 +0900

    feat: Library.jsx 수정 사항 반영

--- client/src/pages/Library.jsx
+++ client/src/pages/Library.jsx
@@ -2,6 +2,7 @@ import { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import confetti from "canvas-confetti";
 import { toast } from "sonner";
+import { motion } from "framer-motion";
 import { MOCK_BOOKS, READING_CALENDAR } from "@/lib/mockData";
 import {
   Plus,
