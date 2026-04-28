3084e9bbfebc82d7a2a4baa5746802d6bd70f69b
Minseo Shin <minseo9451@gmail.com>
2026-04-28 17:42:39 +0900

    fix: express 5 route wildcard syntax

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.use(
 app.use(express.static(path.join(__dirname, "dist/public")));
 
 // SPA 라우팅 (모든 요청을 index.html로 보내 React가 처리하게 함)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/public/index.html"));
+app.get("(.*)", (req, res) => {
+  res.sendFile(path.resolve("dist/public/index.html"));
 });
 
 const PORT = 80;
